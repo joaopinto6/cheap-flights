104d543bd499c5a07cef0fde4442f696cd6783e5
Joao Pinto <joaomeypinto6@tecnico.ulisboa.pt>
2022-12-23 03:44:09 +0000

    SHREK

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,19 +2,6 @@ const fs = require('fs');
 
 export const handler = async () => {
 
-    const old_data = fs.readFileSync('./netlify/functions/file.txt', 'utf8');
-    
-    var num = Number(old_data)
-    num++
-    const num_str = num.toString()
-
-    fs.writeFile('./netlify/functions/file.txt', num_str, { flag: 'w+' }, err => {
-      if (err) {
-        console.error(err);
-      }
-      // file written successfully
-    });
-
     const data = fs.readFileSync('./netlify/functions/file.txt', 'utf8');
 
     return {
