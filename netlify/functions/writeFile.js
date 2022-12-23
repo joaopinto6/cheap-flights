db812a7d043d794c11c39544121edf5702d6b5df
Joao Pinto <joaomeypinto6@tecnico.ulisboa.pt>
2022-12-23 22:32:35 +0000

    horas

--- netlify/functions/writeFile.js
+++ netlify/functions/writeFile.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 
 export const handler = async () => {
-    content = '+1'
-    fs.writeFile('./netlify/functions/file.txt', content, { flag: 'a+' }, err => {});
-    const data = fs.readFileSync('./netlify/functions/file.txt', 'utf8');
-    console.log(data)
+    
+    let date_ob = new Date();
+
+    // current hours
+    let hours = date_ob.getHours();
+
+    // current minutes
+    let minutes = date_ob.getMinutes();
+
+    // prints time in HH:MM format
+    console.log(hours + ":" + minutes);
+    
+    // content = '+1'
+    // fs.writeFile('./netlify/functions/file.txt', content, { flag: 'a' }, err => {});
+    // const data = fs.readFileSync('./netlify/functions/file.txt', 'utf8');
+    // console.log(data)
     
     return {
       statusCode: 200,
